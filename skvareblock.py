13e90780b0f3b8912ddb4a2ae4424f40aecd0a1d
John Ramsden <johnramsden@riseup.net>
2018-08-14 10:54:31 -0700

    Add ip ranges to iplist

--- skvareblock.py
+++ skvareblock.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+import shutil
+import subprocess
+import sys
 import urllib.request
 
 firewall_ipset = "blockipset"
@@ -20,8 +25,9 @@ def sanitize_droplist(ips: list, comment_characters: list) -> list:
 
     for ip in ips:
         ip_line = next((ip.split(c)[0] for c in comment_characters if c in ip), ip)
-        if ip_line.strip() and not any(nb in ip_line for nb in never_block):
-            sanitized_ips.append(ip_line)
+        stripped_line = ip_line.strip()
+        if stripped_line and not any(nb in ip_line for nb in never_block):
+            sanitized_ips.append(stripped_line)
 
     return sanitized_ips
 
@@ -33,15 +39,105 @@ def fetch_droplist(url: str) -> list:
     return data.decode('utf-8').split('\n')
 
 
+class Ipset:
+
+    def __init__(self, name: str, method: str, data_type: str, extra_args: list=None):
+        if not shutil.which("ipset"):
+            raise FileNotFoundError
+
+        self.name = name
+        self.method = method
+        self.data_type = data_type
+        self.extra_args = extra_args
+
+    def create(self):
+        arguments = ['ipset', 'create', self.name, f"{self.method}:{self.data_type}"]
+
+        if self.extra_args:
+            arguments.extend(self.extra_args)
+
+        try:
+            ipset = subprocess.run(arguments, check=True)
+        except subprocess.CalledProcessError:
+            raise
+
+        return ipset
+
+    def add_ips(self, ips: list):
+        arguments = ['ipset', 'add', self.name]
+
+        for ip in ips:
+            try:
+                subprocess.run(arguments + [ip], check=True)
+            except subprocess.CalledProcessError:
+                raise
+
+    def swap(self, swap_ipset: str):
+        """
+        swap the ipset
+        """
+        arguments = ['ipset', 'swap', self.name, swap_ipset]
+
+        try:
+            subprocess.run(arguments, check=True)
+        except subprocess.CalledProcessError:
+            raise
+
+    def destroy(self):
+        """
+        Destroy the ipset
+        """
+        arguments = ['ipset', 'destroy', self.name]
+
+        try:
+            subprocess.run(arguments, check=True)
+        except subprocess.CalledProcessError:
+            raise
+
+
 def main():
+    try:
+        temp_ipset = Ipset(f"temp_{firewall_ipset}", "hash", "net", extra_args=['-exist'])
+    except FileNotFoundError:
+        sys.exit("ipset command not found")
+
+    try:
+        temp_ipset_return = temp_ipset.create()
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"Failed to create ipset: {e.args}\n{e.stderr}")
+
     ip_list = []
-
     for u in droplist_urls:
-        ip_list.extend(
-            sanitize_droplist(fetch_droplist(u), [';', '#']))
+        for ipl in sanitize_droplist(fetch_droplist(u), [';', '#']):
+            if ipl not in ip_list:
+                ip_list.append(ipl)
 
-    for ip in ip_list:
-        print(ip)
+    try:
+        temp_ipset.add_ips(ip_list)
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"Failed to add ips to ipset: {e.args}\n{e.stderr}")
+
+    # Create main ipset if doesn't exist
+    try:
+        ipset = Ipset(firewall_ipset, "hash", "net", extra_args=['-exist'])
+    except FileNotFoundError:
+        sys.exit("ipset command not found")
+
+    try:
+        ipset_return = ipset.create()
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"Failed to create ipset: {e.args}\n{e.stderr}")
+
+    try:
+        temp_ipset.swap(ipset.name)
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"Failed to swap ipset: {e.args}\n{e.stderr}")
+
+    # Destroy the temp
+    try:
+        temp_ipset.destroy()
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"Failed to destroy ipset: {e.args}\n{e.stderr}")
 
 
 if __name__ == "__main__":
